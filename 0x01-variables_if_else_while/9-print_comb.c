e33f278185cfbd7ef902f007b52206681151e6e8
Blynkxx <oscarkuk@gmail.com>
2022-10-15 17:51:44 -0700

    print combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-
 /**
  * main - print all possible combination of single digits
+ *
  * Return: Always 0
  */
 int main(void)
 {
 	int n;
 
-	for (n = 0; n < 100; n++)
+	for (n = 0; n < 10; n++)
 	{
 		putchar(n + '0');
+	}
 		if (n < 9)
 		{
 			putchar(',');
