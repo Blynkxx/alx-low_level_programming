4a5b3091f4918569fc1bcad0a398003ffe873359
Blynkxx <oscarkuk@gmail.com>
2022-11-02 23:00:47 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - finds sqrt
+ * @n: the number
+ * Return: -1, 0, or sqrt
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	else if (n > 1)
+	{
+		return (_increasevar(1, n));
+	}
+	return (-1);
+}
